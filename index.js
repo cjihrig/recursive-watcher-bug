4139c2b7f59f0647f1031b26ad6038bf5e2b1a8a
cjihrig <cjihrig@gmail.com>
2023-06-12 12:33:22 -0400

    changed everything

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+const { mkdtempSync, watch, writeFileSync } = require('node:fs');
+const { tmpdir } = require('node:os');
+const { join } = require('node:path');
+const tmpDir = mkdtempSync(join(tmpdir(), 'repro-test-'));
+const filename = join(tmpDir, 'test.file');
+const keepalive = setTimeout(() => {
+  throw new Error('timed out');
+}, 60_000);
+
+const watcher = watch(tmpDir, { recursive: true }, (eventType, filename) => {
+  clearTimeout(keepalive);
+  watcher.close();
+  console.log(eventType, filename);
+});
+
+writeFileSync(filename, 'foobar');
