5263ae20844d8612fb27c29fe115283630d77fba
cjihrig <cjihrig@gmail.com>
2023-06-12 11:42:24 -0400

    remove more

--- test/index.js
+++ test/index.js
@@ -1,42 +1,28 @@
-'use strict'
+'use strict';
+const { mkdtempSync, writeFileSync } = require('node:fs');
+const { tmpdir } = require('node:os');
+const { join } = require('node:path');
+const { test } = require('node:test');
+const { FileWatcher } = require('..');
 
-const os = require('os')
-const { mkdtemp, writeFile } = require('fs/promises')
-const { join } = require('path')
-const { test } = require('tap')
-const { FileWatcher } = require('..')
+function getTempDir() {
+  return mkdtempSync(join(tmpdir(), 'repro-test-'));
+}
 
-test('should throw an error if there is no path argument', async ({ throws, plan }) => {
-  plan(1)
-  throws(() => new FileWatcher({}), 'path option is required')
-})
+test('should emit event if file is updated', (_, done) => {
+  const tmpDir = getTempDir();
+  const filename = join(tmpDir, 'test.file');
+  const fileWatcher = new FileWatcher({ path: tmpDir });
 
-test('should emit event if file is updated', async ({ end }) => {
-  const tmpDir = await mkdtemp(join(os.tmpdir(), 'plt-utils-test-'))
-  const filename = join(tmpDir, 'test.file')
-  const fileWatcher = new FileWatcher({ path: tmpDir })
-
-  fileWatcher.once('update', async () => {
-    await fileWatcher.stopWatching()
-    end()
-  })
-  fileWatcher.startWatching()
-
-  writeFile(filename, 'foobar')
-})
-
-test('should not call fs watch twice', async ({ pass, plan, end }) => {
-  const tmpDir = await mkdtemp(join(os.tmpdir(), 'plt-utils-test-'))
-  const filename = join(tmpDir, 'test.file')
-  const fileWatcher = new FileWatcher({ path: tmpDir })
+  // Use a timer to keep the event loop alive. This shouldn't be needed though.
+  const keepalive = setTimeout(() => {}, 20_000);
 
   fileWatcher.once('update', async () => {
-    await fileWatcher.stopWatching()
-    await fileWatcher.stopWatching()
-    end()
+    await fileWatcher.stopWatching();
+    clearTimeout(keepalive);
+    done();
   })
-  fileWatcher.startWatching()
-  fileWatcher.startWatching()
 
-  writeFile(filename, 'foobar')
-})
+  fileWatcher.startWatching();
+  writeFileSync(filename, 'foobar');
+});
